7842505d14a1730f485f277dde8a4dd83549a643
Julia Taiba <j.taiba.97@hotmail.com>
2024-11-01 13:00:09 +0100

    ChangeBtn is functionable. ErrorMsg added if changed input is empty.

--- index.js
+++ index.js
@@ -23,6 +23,59 @@ document.getElementById('create').addEventListener('click', function (e) {
       `
 
     contactList.appendChild(contactItem);
+    btnEvents(contactItem);
+
+    // Redigera kontakt.
+
+    function btnEvents(contactItem) {
+
+    contactItem.querySelector('.changeBtn').addEventListener('click', function () {
+      let contactText = contactItem.firstChild.textContent.trim();
+      let [currentFirstName, currentPhoneNumber] = contactText.split(' ');
+
+      let firstNameInput = document.createElement('input');
+      firstNameInput.type = 'text';
+      firstNameInput.value = currentFirstName;
+
+      let firstPhoneNumberInput = document.createElement('input');
+      firstPhoneNumberInput.type = 'text';
+      firstPhoneNumberInput.value = currentPhoneNumber;
+
+      contactItem.innerHTML = '';
+
+      contactItem.appendChild(firstNameInput);
+      contactItem.appendChild(firstPhoneNumberInput);
+      
+      let saveBtn = document.createElement('button');
+      saveBtn.textContent = 'Save';
+      contactItem.appendChild(saveBtn);
+
+      saveBtn.addEventListener('click', function () {
+        if (firstNameInput.value === "" || firstPhoneNumberInput.value === "") {
+          errorMessage.textContent = "Can´t be empty!";
+          errorMessage.style.display = 'block';
+        } else {
+          errorMessage.style.display = 'none';
+
+          let updatedFirstName = firstNameInput.value;
+          let updatedPhoneNumber = firstPhoneNumberInput.value;
+
+          contactItem.innerHTML = `
+          ${updatedFirstName} ${updatedPhoneNumber}
+          <button class="changeBtn">Change</button>
+          <button class="deleteBtn">Delete</button>        
+          `;
+          btnEvents(contactItem);
+        }
+      }) 
+    })
+
+    // Radera enskild kontakt.
+
+    contactItem.querySelector('.deleteBtn').addEventListener('click', function () {
+      contactList.removeChild(contactItem);
+    });
+  }
 
     document.getElementById('firstName').value = "";
     document.getElementById('phoneNumber').value = "";
@@ -44,3 +97,4 @@ document.getElementById('delete').addEventListener('click', function (e) {
     errorMessage.style.display = 'none';
   }
 })
+
